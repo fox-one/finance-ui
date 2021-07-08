e5386fc68ef9f744a162a5e6b561db375e1a72ad
bobby.li <bobby.lhan@gmail.com>
2021-07-08 12:46:12 +0800

    [@FOXONE/FINANCE-UI]: 0.1.0-beta.3

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,8 +2,8 @@
 
 module.exports = {
   env: {
-    'browser': true,
-    'es6': true
+    browser: true,
+    es6: true
   },
   extends: [
     'eslint:recommended',
@@ -12,8 +12,8 @@ module.exports = {
     'plugin:prettier/recommended'
   ],
   globals: {
-    'Atomics': 'readonly',
-    'SharedArrayBuffer': 'readonly'
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
   },
   parser: 'vue-eslint-parser',
   parserOptions: {
@@ -30,47 +30,75 @@ module.exports = {
   ],
   rules: {
     'vue/html-indent': ['warn', 2],
-    'vue/html-self-closing': ['warn', {
-      'html': {
-        'void': 'never',
-        'normal': 'always',
-        'component': 'always'
-      },
-      'svg': 'always',
-      'math': 'always'
-    }],
-    'vue/html-quotes': ['warn', 'double', { 'avoidEscape': true }],
-    'vue/singleline-html-element-content-newline': ['off'],
-    '@typescript-eslint/indent': ['warn', 2],
-    '@typescript-eslint/no-empty-interface': ['off'],
+    'vue/html-self-closing': [
+      'warn',
+      {
+        html: {
+          void: 'never',
+          normal: 'always',
+          component: 'always'
+        },
+        svg: 'always',
+        math: 'always'
+      }
+    ],
+    'vue/html-quotes': [
+      'warn',
+      'double',
+      { avoidEscape: true }
+    ],
+    'vue/singleline-html-element-content-newline': [
+      'off'
+    ],
+    // '@typescript-eslint/indent': ['warn', 2],
+    '@typescript-eslint/no-empty-interface': [
+      'off'
+    ],
     '@typescript-eslint/camelcase': ['off'],
-    '@typescript-eslint/restrict-plus-operands': ['warn'],
+    '@typescript-eslint/restrict-plus-operands': [
+      'warn'
+    ],
     '@typescript-eslint/array-type': ['off'],
-    '@typescript-eslint/no-use-before-define': ['off'],
-    '@typescript-eslint/no-angle-bracket-type-assertion': ['off'],
-    '@typescript-eslint/interface-name-prefix': ['off'],
-    '@typescript-eslint/explicit-member-accessibility': ['warn'],
-    '@typescript-eslint/consistent-type-assertions': ['warn'],
-    '@typescript-eslint/no-inferrable-types': ['warn'],
-    'no-console': ['error', { 'allow': ['warn', 'error', 'info'] }],
-    'semi': ['error', 'always'],
+    '@typescript-eslint/no-use-before-define': [
+      'off'
+    ],
+    '@typescript-eslint/no-angle-bracket-type-assertion': [
+      'off'
+    ],
+    '@typescript-eslint/interface-name-prefix': [
+      'off'
+    ],
+    '@typescript-eslint/explicit-member-accessibility': [
+      'warn'
+    ],
+    '@typescript-eslint/consistent-type-assertions': [
+      'warn'
+    ],
+    '@typescript-eslint/no-inferrable-types': [
+      'warn'
+    ],
+    'no-console': [
+      'error',
+      { allow: ['warn', 'error', 'info'] }
+    ],
+    semi: ['error', 'always'],
     'prefer-spread': ['warn'],
     'no-unused-vars': ['off'],
     'no-extra-semi': ['warn'],
-    'quotes': ['error', 'single'],
+    quotes: ['error', 'single'],
     'linebreak-style': ['warn', 'unix'],
     'prettier/prettier': [
       'warn',
       {
-        'printWidth': 50,
-        'tabWidth': 2,
-        'singleQuote': true,
-        'jsxSingleQuote': true,
-        'semi': true,
-        'trailingComma': 'none',
-        'endOfLine': 'auto',
-        'arrowParens': 'avoid',
-        'rangeEnd': 0
+        printWidth: 50,
+        tabWidth: 2,
+        singleQuote: true,
+        jsxSingleQuote: true,
+        semi: true,
+        trailingComma: 'none',
+        endOfLine: 'auto',
+        arrowParens: 'avoid',
+        rangeEnd: 0
       }
     ]
   }
--- omni.config.js
+++ omni.config.js
@@ -58,7 +58,7 @@ module.exports = {
     preflight: {
       test: true, // 发布前是否进行单元测试 (whether or not process unit-test)
       eslint: true, // 发布前是否进行eslint检测 (whether or not process eslint checking)
-      prettier: false, // 发布前是否进行prettier检测 (whether or not process prettier checking)
+      prettier: true, // 发布前是否进行prettier检测 (whether or not process prettier checking)
       stylelint: true, // 发布前是否进行stylelint检测 (whether or not process stylelint checking)
       commitlint: true, // 发布前是否进行commitlint检测 (whether or not process commitlint checking)
       branch: 'master' // 发布前进行分支检测，设置为空字符串则不会检测 (only can release in this branch, set empty string to ignore this check)
